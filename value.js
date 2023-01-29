cbde011b121eade0418ef2fa2a4b1a088ec792b7
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-01-29 16:32:47 +0300

    Added new file value.js

---
+++ value.js
@@ -0,0 +1,7 @@
+const VALUE1 = 10
+let value2 = Math.floor(Math.random() * 51)
+console.log(value2)
+console.log(VALUE1 + value2/5 + 17)
+
+/* в приоритете сначала выполняются операторы умножения и деления, 
+а затем уже сложения, вычитания
