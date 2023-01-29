d9c10cd36c2dd6c580ab9d4250c9dd5f3914bbdf
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-01-29 14:25:50 +0300

    Added file task_score.js

---
+++ task_score.js
@@ -0,0 +1,7 @@
+let intro = 100;
+let git = 269;
+let js = 565;
+let average = (intro + git + js) / 3;
+console.log(average);
+let studentName = 'VasyaEnotov';
+console.log(`${studentName} task_score is ${average}`);
