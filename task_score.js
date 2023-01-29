18cc0d77e882ba311169e7d7adf0e04811dd29a5
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-01-29 16:08:03 +0300

    Added method slice to task_score.js

--- task_score.js
+++ task_score.js
@@ -3,5 +3,8 @@ let git = 269;
 let js = 565;
 let average = (intro + git + js) / 3;
 console.log(average);
-let studentName = 'VasyaEnotov';
-console.log(`${studentName} task_score is ${average}`);
+let studentName = 'Vasya';
+let studentSurname = 'Enotov';
+console.log(
+  `${studentName.slice(0, 1)}.${studentSurname} task_score is ${average}`
+);
